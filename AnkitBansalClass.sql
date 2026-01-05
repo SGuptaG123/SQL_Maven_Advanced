cb220399d881502146a80bb383fc78cf3205f0ab
Sandeep Gupta <42830159+SGuptaG123@users.noreply.github.com>
2026-01-05 22:50:20 +0530

    Add files via upload

--- AnkitBansalClass.sql
+++ AnkitBansalClass.sql
@@ -491,10 +491,99 @@ Select *,
 from empDept
 
 
+--5-Jan-2026---9---------------------Day 12 - Find Winner in each group----------------------------
+--Write a SQL query to find the winner in each group
+--The winner in each group is the player who scored the maximum total points within the group
+--In case of tie the lowest player_id wins
+Use AnkitBansalClass
+
+create table players
+					(player_id int,
+					group_id int)
+
+insert into players values (15,1);
+insert into players values (25,1);
+insert into players values (30,1);
+insert into players values (45,1);
+insert into players values (10,2);
+insert into players values (35,2);
+insert into players values (50,2);
+insert into players values (20,3);
+insert into players values (40,3);
+
+create table matches
+						(
+						match_id int,
+						first_player int,
+						second_player int,
+						first_score int,
+						second_score int)
+
+insert into matches values (1,15,45,3,0);
+insert into matches values (2,30,25,1,2);
+insert into matches values (3,30,15,2,0);
+insert into matches values (4,40,20,5,2);
+insert into matches values (5,35,50,1,1);
 
 
+Select * from players;
+Select * from matches;
+
+with match_details as (
+									Select match_id, first_player as player_id, first_score as score from matches 
+									Union All
+									Select match_id, second_player, second_score from matches),
+		player_score as (
+									Select group_id, p.player_id, sum(score) as score
+									from players p
+									join match_details md
+											on (p.player_id = md.player_id)
+									group by group_id, p.player_id),
+		
+		player_win_rnk as (
+									select *,
+												ROW_NUMBER() over (Partition by group_id order by score desc, player_id ASC) as rnk
+									from player_score)
+Select group_id, player_id, score
+from player_win_rnk
+where rnk = 1
+
+--6-Jan-2026---10---------------------Day 13 - ----------------------------
+--Market Analysis, Write a SQL query to find for each seller, whether the brand of the second item (by Date) they sold is their fovorite
+--If a seller sold less than two items, report the answer for the seller as no. o/p
+--Seller id			2nd_item_fav_brand
+--1						yes/No
+--2						yes/No
+
+create table users13 (
+						user_id         int     ,
+						 join_date       date    ,
+						 favorite_brand  varchar(50));
+
+ create table orders13 (order_id       int     ,
+									 order_date     date    ,
+									 item_id        int     ,
+									 buyer_id       int     ,
+									 seller_id      int );
+
+ create table items13 (
+			 item_id        int     ,
+			 item_brand     varchar(50));
 
 
+ insert into users13 values (1,'2019-01-01','Lenovo'),(2,'2019-02-09','Samsung'),(3,'2019-01-19','LG'),(4,'2019-05-21','HP');
+ insert into items13 values (1,'Samsung'),(2,'Lenovo'),(3,'LG'),(4,'HP');
+ insert into orders13 values 
+							(1,'2019-08-01',4,1,2),
+							(2,'2019-08-02',2,1,3),
+							(3,'2019-08-03',3,2,3),
+							(4,'2019-08-04',1,4,2),
+							(5,'2019-08-04',1,3,4),
+							(6,'2019-08-05',2,2,4);
+
+Select * from users13
+Select * from items13
+Select * from orders13
 
 
 
